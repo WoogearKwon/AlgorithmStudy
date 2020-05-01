081fba5db69e5397efc4d61d6181caf11894a1c2
Woogear Kwon <techkwon1717@hanmail.net>
2020-05-01 18:35:16 +0900

    test

--- src/TestField.java
+++ src/TestField.java
@@ -2,17 +2,15 @@ import com.sun.org.apache.xml.internal.utils.IntStack;
 
 import java.util.Arrays;
 
-public class DataStructure {
+public class TestField {
 
     public static void main(String[] args) {
         int[] x = {7,6,104,5,6,3,8,9,11,1,4,3,2,3,5,53,3,4};
         int n = x.length;
 
-        Arrays.sort(x);
-
+        println("====finished====");
         println(Arrays.toString(x));
     }
-
 
 
     static void println(String txt) {
