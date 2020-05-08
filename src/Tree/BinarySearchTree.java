902a9700948c1668d9392ff88bad4ce3de03a633
Woogear Kwon <techkwon1717@hanmail.net>
2020-05-08 16:53:52 +0900

    이진검색트리 업데이트

--- src/Tree/BinarySearchTree.java
+++ src/Tree/BinarySearchTree.java
@@ -81,17 +81,111 @@ public class BinarySearchTree<K, V> {
     private void addNode(Node<K,V> node, K key, V data) {
         int cond = comp(key, node.getKey());
         if (cond == 0)
-            return;
-        else if (cond < 0) {
+            return;             // key가 이진검색트리에 이미 있음
+        else if (cond < 0) { // 음수: key < node.getKey()
             if (node.left == null)
-                node.left = new Node<K,V>(key, data, null, null);
+                node.left = new Node<>(key, data, null, null);
             else
-                addNode(node.left, key, data);
+                addNode(node.left, key, data); // 왼쪽 서브트리로 추가
         } else {
             if (node.right == null)
-                node.right = new Node<K,V>(key, data, null, null);
+                node.right = new Node<>(key, data, null, null);
             else
-                addNode(node.right, key, data);
+                addNode(node.right, key, data); // 오른쪽 서브트리로 추가
         }
+    }
+
+    public void add(K key, V data) {
+        if (root == null)
+            root = new Node<>(key, data, null, null);
+        else
+            addNode(root, key, data);
+    }
+
+    // 키 값이 key인 노드를 삭제 (과정이 복잡함)
+    public boolean remove(K key) {
+        Node<K, V> p = root;                    // 스캔 중인 노드
+        Node<K, V> parent = null;               // 스캔 중인 노드의 부모 노드
+        boolean isLeftChild = true;             // p는 parent의 왼쪽 자식 노드인가?
+
+        while (true) {                          // *** 노드 찾기 (p의 키값이 key와 동일할 때까지 반복) ***
+            if (p == null)                      // 더 이상 진행하지 않으면
+                return false;                   // 그 키 값은 없음
+            int cond = comp(key, p.getKey());   // key와 노드 p의 키 값을 비교
+            if (cond == 0)                      // 같으면
+                break;                          // 검색 성공
+            else {
+                parent = p;                     // 가지로 내려가기 전에 부모를 설정
+                if (cond < 0) {                 // key 쪽이 작으면
+                    isLeftChild = true;         // 왼쪽 자식으로 내려감
+                    p = p.left;                 // 왼쪽 서브트리에서 검색
+                } else {                        // key 쪽이 크면
+                    isLeftChild = false;        // 오른쪽 자식으로 내려감
+                    p = p.right;                // 오른쪽 서브 트리에서 검색
+                }
+            }
+        }
+
+        if (p.left == null) {                   // 1. 왼쪽 자식이 없는 경우 (양쪽 자식이 모두 없는 경우도 해당)
+            if (p == root)
+                root = p.right;
+            else if (isLeftChild)
+                parent.left = p.right;          // 부모의 왼쪽 포인터가 오른쪽 자식을 가리킴
+            else
+                parent.right = p.right;         // 부모의 오른쪽 포인터가 오른쪽 자식을 가리킴
+
+        } else if (p.right == null) {           // 2. 오른쪽 자식이 없는 경우
+            if (p == root)
+                root = p.left;
+            else if (isLeftChild)
+                parent.left = p.left;           // 부모의 왼쪽 포인터가 왼쪽 자식을 가리킴
+            else
+                parent.right = p.left;          // 부모의 오른쪽 포인터가 왼쪽 자식을 가리킴
+
+        } else {                                // 양쪽 자식이 모두 있는 경우
+            parent = p;
+            Node<K, V> left = p.left;           // 서브 트리 가운데 가장 큰 노드
+            isLeftChild = true;
+            while (left.right != null) {        // 가장 큰 노드 left를 검색 (left.right이 없을 때 까지 반복)
+                parent = left;
+                left = left.right;
+                isLeftChild = false;
+            }
+            p.key = left.key;                   // left의 키 값을 p로 옮김
+            p.data = left.data;                 // left의 데이터를 p로 옮김
+            if (isLeftChild)
+                parent.left = left.left;        // left를 삭제
+            else
+                parent.right = left.left;       // left를 삭제
+        }
+        return true;
+    }
+
+    // node를 루트로 하는 서브 트리의 노드를 키 값의 오름차순으로 출력
+    // 출력순서: 왼쪽 자식 ->부모 -> 오른쪽 자식
+    private void printSubTree(Node node) {
+        if (node != null) {
+            printSubTree(node.left);							// 왼쪽 서브 트리를 키 값의 오름차순으로 출력
+            System.out.println(node.key + " " + node.data);		// node를 출력
+            printSubTree(node.right);							// 오른쪽 서브 트리를 키 값의 오름차순으로 출력
+        }
+    }
+
+    public void printSubTreeDown(Node node) {
+        if (node != null) {
+            printSubTreeDown(node.right);
+            System.out.println(node.key + " " + node.data);
+            printSubTreeDown(node.left);
+        }
+    }
+
+    // 모든 노드를 키 값의 오름차순으로 출력
+    public void print() {
+        printSubTree(root);
+    }
+
+    // 모든 노드를 키 값의 오륾차순으로 출력
+    public void printReverse() {
+        printSubTreeDown(root);
     }
 }
